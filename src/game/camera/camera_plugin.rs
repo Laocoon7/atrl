54a2cbc66eda88453785ffb609a348760c3af8b8
Wesley Feldsine <laocoon7@gmail.com>
2022-11-07 11:19:06 -0800

    give camera it's own plugin

---
+++ src/game/camera/camera_plugin.rs
@@ -0,0 +1,8 @@
+use crate::game::prelude::*;
+
+pub struct CameraPlugin;
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_main_camera);
+    }
+}
