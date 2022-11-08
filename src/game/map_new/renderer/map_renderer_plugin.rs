0566604de5cfc1a757e76c0dffc735926822fd83
Wesley Feldsine <laocoon7@gmail.com>
2022-11-08 07:41:32 -0800

    map stuff

--- src/game/map_new/renderer/map_renderer_plugin.rs
+++ src/game/map_new/renderer/map_renderer_plugin.rs
@@ -1,1 +1,11 @@
+use crate::game::prelude::*;
 
+pub struct MapRendererPlugin<T> {
+    pub state_running: T,
+}
+
+impl<T: StateNext> Plugin for MapRendererPlugin<T> {
+    fn build(&self, app: &mut App) {
+        app.add_system(update_map_tiles.run_in_state(self.state_running.clone()));
+    }
+}
