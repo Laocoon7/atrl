00970d9cedbed47dfb2b12f9bb0c201accea8062
Jacob LeCoq <lecoqjacob@gmail.com>
2022-11-08 16:06:56 -0600

    pull game_ctx in procgen plugin

--- src/game/game_context.rs
+++ src/game/game_context.rs
@@ -1,12 +1,13 @@
 use crate::game::prelude::*;
+use once_cell::sync::Lazy;
 use parking_lot::{Mutex, MutexGuard};
 
 pub struct GameContext {
-    pub random: Mutex<Random>,
+    pub random: Lazy<Mutex<Random>>,
 }
 
 impl Default for GameContext {
-    fn default() -> Self { Self { random: Mutex::new(Random::from_entropy()) } }
+    fn default() -> Self { Self { random: Lazy::new(|| Mutex::new(Random::from_entropy())) } }
 }
 
 impl GameContext {
