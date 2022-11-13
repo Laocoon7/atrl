511f4514a1cb60fc52d2d8eaa021c89533304e17
Wesley Feldsine <laocoon7@gmail.com>
2022-11-13 03:45:19 -0800

    map system updates

---
+++ crates/map/src/systems/load_themes_into_theme_server.rs
@@ -0,0 +1,13 @@
+use crate::prelude::*;
+
+const THEME_DIR: &str = "assets/themes/";
+
+pub fn load_themes_into_theme_server(
+    mut theme_server: ResMut<ThemeServer>,
+) {
+    theme_server.tilesets.clear();
+    theme_server.themes.clear();
+
+    
+
+}
