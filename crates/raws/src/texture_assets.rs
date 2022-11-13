7ff88570e7d7370037e5ec6e7cb7f8fd1e00cada
Wesley Feldsine <laocoon7@gmail.com>
2022-11-13 04:12:17 -0800

    finish removing white_pixel.png

--- crates/raws/src/texture_assets.rs
+++ crates/raws/src/texture_assets.rs
@@ -6,7 +6,4 @@ pub struct TextureAssets {
     #[asset(texture_atlas(tile_size_x = 8., tile_size_y = 8., columns = 16, rows = 16))]
     #[asset(path = "tilesets/terminal8x8_transparent.png")]
     pub terminal8x8_atlas: Handle<TextureAtlas>,
-
-    #[asset(path = "images/white_pixel.png")]
-    pub white_pixel: Handle<Image>,
 }
