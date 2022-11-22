bc59471d0b7d69a921f44b98ac67bf71a2574f37
Jacob LeCoq <lecoqjacob@gmail.com>
2022-11-21 19:59:45 -0600

    remove all Query<&Map>

--- crates/game/src/player/systems/player_input.rs
+++ crates/game/src/player/systems/player_input.rs
@@ -3,25 +3,18 @@ use crate::prelude::*;
 pub fn player_input(
     state: Res<TurnState>,
     mut commands: Commands,
-    map_q: Query<&Map>,
-    mut query: Query<
-        (&mut Transform, &WorldPosition, &Movement, &ActionState<PlayerAction>),
-        With<Player>,
-    >,
+    manager: Res<MapManager>,
+    mut query: Query<(&mut Transform, &Movement, &ActionState<PlayerAction>), With<Player>>,
 ) {
-    for (mut position, world_position, movement_component, action_state) in query.iter_mut() {
+    for (mut position, movement_component, action_state) in query.iter_mut() {
         for input_direction in PlayerAction::DIRECTIONS {
             if action_state.just_pressed(input_direction) {
                 if let Some(direction) = input_direction.direction() {
                     let mut position_vec = position.get();
                     position_vec += direction.coord();
 
-                    if let Some(map) =
-                        map_q.iter().find(|map| map.world_position == *world_position)
-                    {
-                        if map
-                            .can_move_through((position_vec.x, position_vec.y), movement_component)
-                        {
+                    if let Some(map) = manager.get_current_map() {
+                        if map.can_move_through(position_vec, movement_component) {
                             position.set_value(position_vec);
                             info!(
                                 "Player moved to {} {:?} {:?}",
