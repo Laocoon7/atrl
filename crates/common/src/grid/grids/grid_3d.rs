ab38231e29e67487d37260bfac2b41a8cf6e9464
Wesley Feldsine <laocoon7@gmail.com>
2022-11-29 18:59:16 -0800

    grid_3d::new_XXX

--- crates/common/src/grid/grids/grid_3d.rs
+++ crates/common/src/grid/grids/grid_3d.rs
@@ -6,14 +6,39 @@ pub struct Grid3d<T: GridParam, const LAYER_COUNT: usize> {
 }
 
 impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
-    
-    pub fn new_default(size: impl Size2d) -> Self
-        where T: Default {
-        let count = size.count();
+    pub fn new_clone(size: impl Size2d, value: T) -> Self
+    where T: Clone {
         let mut layers = Vec::new();
         for _ in 0..LAYER_COUNT {
-            let mut layer = Grid::<T>::new_default(size);
-            layer.resize_with(count, Default::default);
+            let layer = Grid::new_clone(size, value.clone());
+            layers.push(layer);
+        }
+
+        Self {
+            size: size.as_uvec2(),
+            layers: layers.try_into().unwrap_or_else(|_v| panic!("Something went wrong!")),
+        }
+    }
+
+    pub fn new_copy(size: impl Size2d, value: T) -> Self
+    where T: Copy {
+        let mut layers = Vec::new();
+        for _ in 0..LAYER_COUNT {
+            let layer = Grid::new_copy(size, value);
+            layers.push(layer);
+        }
+
+        Self {
+            size: size.as_uvec2(),
+            layers: layers.try_into().unwrap_or_else(|_v| panic!("Something went wrong!")),
+        }
+    }
+
+    pub fn new_default(size: impl Size2d) -> Self
+    where T: Default {
+        let mut layers = Vec::new();
+        for _ in 0..LAYER_COUNT {
+            let layer = Grid::new_default(size);
             layers.push(layer);
         }
 
@@ -31,7 +56,10 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
             for coord in size.iter() {
                 cells.push(f((index, coord)));
             }
-            layers.push(Grid::<T> {size: size.as_uvec2(), cells});
+            layers.push(Grid::<T> {
+                size: size.as_uvec2(),
+                cells,
+            });
         }
 
         Self {
@@ -39,10 +67,18 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
             layers: layers.try_into().unwrap_or_else(|_v| panic!("Something went wrong!")),
         }
     }
-    
 
-    pub fn blit_clone<ToLayerId: Into<usize>, FromLayerId: Into<usize>>(&mut self, to_layer_id: ToLayerId, to: impl Point2d, source: &Self, from_layer_id: FromLayerId, from: impl Point2d, size: impl Size2d)
-        where T: Clone {
+    pub fn blit_clone<ToLayerId: Into<usize>, FromLayerId: Into<usize>>(
+        &mut self,
+        to_layer_id: ToLayerId,
+        to: impl Point2d,
+        source: &Self,
+        from_layer_id: FromLayerId,
+        from: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Clone,
+    {
         if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
             if let Some(from_layer) = source.get_grid_by_layer(from_layer_id) {
                 to_layer.blit_clone(to, from_layer, from, size);
@@ -50,41 +86,82 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
         }
     }
 
-    pub fn blit_copy<ToLayerId: Into<usize>, FromLayerId: Into<usize>>(&mut self, to_layer_id: ToLayerId, to: impl Point2d, source: &Self, from_layer_id: FromLayerId, from: impl Point2d, size: impl Size2d)
-        where T: Copy {
-            if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
-                if let Some(from_layer) = source.get_grid_by_layer(from_layer_id) {
-                    to_layer.blit_copy(to, from_layer, from, size);
-                }
+    pub fn blit_copy<ToLayerId: Into<usize>, FromLayerId: Into<usize>>(
+        &mut self,
+        to_layer_id: ToLayerId,
+        to: impl Point2d,
+        source: &Self,
+        from_layer_id: FromLayerId,
+        from: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Copy,
+    {
+        if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
+            if let Some(from_layer) = source.get_grid_by_layer(from_layer_id) {
+                to_layer.blit_copy(to, from_layer, from, size);
             }
+        }
     }
 
-    pub fn blit_clone_from_2d<ToLayerId: Into<usize>>(&mut self, to_layer_id: ToLayerId, to: impl Point2d, source: &Grid<T>, from: impl Point2d, size: impl Size2d)
-        where T: Clone {
-            if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
-                to_layer.blit_clone(to, source, from, size);
-            }
+    pub fn blit_clone_from_2d<ToLayerId: Into<usize>>(
+        &mut self,
+        to_layer_id: ToLayerId,
+        to: impl Point2d,
+        source: &Grid<T>,
+        from: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Clone,
+    {
+        if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
+            to_layer.blit_clone(to, source, from, size);
+        }
     }
 
-    pub fn blit_copy_from_2d<ToLayerId: Into<usize>>(&mut self, to_layer_id: ToLayerId, to: impl Point2d, source: &Grid<T>, from: impl Point2d, size: impl Size2d)
-        where T: Copy {
-            if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
-                to_layer.blit_copy(to, source, from, size);
-            }
+    pub fn blit_copy_from_2d<ToLayerId: Into<usize>>(
+        &mut self,
+        to_layer_id: ToLayerId,
+        to: impl Point2d,
+        source: &Grid<T>,
+        from: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Copy,
+    {
+        if let Some(to_layer) = self.get_grid_by_layer_mut(to_layer_id) {
+            to_layer.blit_copy(to, source, from, size);
+        }
     }
 
-    pub fn blit_clone_to_2d<FromLayerId: Into<usize>>(&self, from_layer_id: FromLayerId, from: impl Point2d, dest: &mut Grid<T>, to: impl Point2d, size: impl Size2d)
-        where T: Clone {
-            if let Some(from_layer) = self.get_grid_by_layer(from_layer_id) {
-                dest.blit_clone(to, from_layer, from, size);
-            }
+    pub fn blit_clone_to_2d<FromLayerId: Into<usize>>(
+        &self,
+        from_layer_id: FromLayerId,
+        from: impl Point2d,
+        dest: &mut Grid<T>,
+        to: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Clone,
+    {
+        if let Some(from_layer) = self.get_grid_by_layer(from_layer_id) {
+            dest.blit_clone(to, from_layer, from, size);
+        }
     }
 
-    pub fn blit_copy_to_2d<FromLayerId: Into<usize>>(&self, from_layer_id: FromLayerId, from: impl Point2d, dest: &mut Grid<T>, to: impl Point2d, size: impl Size2d)
-        where T: Copy {
-            if let Some(from_layer) = self.get_grid_by_layer(from_layer_id) {
-                dest.blit_copy(to, from_layer, from, size);
-            }
+    pub fn blit_copy_to_2d<FromLayerId: Into<usize>>(
+        &self,
+        from_layer_id: FromLayerId,
+        from: impl Point2d,
+        dest: &mut Grid<T>,
+        to: impl Point2d,
+        size: impl Size2d,
+    ) where
+        T: Copy,
+    {
+        if let Some(from_layer) = self.get_grid_by_layer(from_layer_id) {
+            dest.blit_copy(to, from_layer, from, size);
+        }
     }
 
     #[inline]
@@ -135,7 +212,7 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
 
     #[inline]
     pub fn get<LayerId: Into<usize>>(&self, layer_id: LayerId, index: impl Point2d) -> Option<&T> {
-        if let Some(layer) = self.get_grid_by_layer(layer_id){
+        if let Some(layer) = self.get_grid_by_layer(layer_id) {
             layer.get(index)
         } else {
             None
@@ -143,7 +220,11 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
     }
 
     #[inline]
-    pub fn get_mut<LayerId: Into<usize>>(&mut self, layer_id: LayerId, index: impl Point2d) -> Option<&mut T> {
+    pub fn get_mut<LayerId: Into<usize>>(
+        &mut self,
+        layer_id: LayerId,
+        index: impl Point2d,
+    ) -> Option<&mut T> {
         if let Some(layer) = self.get_grid_by_layer_mut(layer_id) {
             layer.get_mut(index)
         } else {
@@ -157,12 +238,21 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
     }
 
     #[inline]
-    pub fn get_mut_unchecked<LayerId: Into<usize>>(&mut self, layer_id: LayerId, index: impl Point2d) -> &mut T {
+    pub fn get_mut_unchecked<LayerId: Into<usize>>(
+        &mut self,
+        layer_id: LayerId,
+        index: impl Point2d,
+    ) -> &mut T {
         self.layers[layer_id.into()].get_mut_unchecked(index)
     }
 
     #[inline]
-    pub fn set<LayerId: Into<usize>>(&mut self, layer_id: LayerId, index: impl Point2d, value: T) -> Option<T> {
+    pub fn set<LayerId: Into<usize>>(
+        &mut self,
+        layer_id: LayerId,
+        index: impl Point2d,
+        value: T,
+    ) -> Option<T> {
         if let Some(layer) = self.get_grid_by_layer_mut(layer_id) {
             layer.set(index, value)
         } else {
@@ -171,10 +261,14 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
     }
 
     #[inline]
-    pub fn set_unchecked<LayerId: Into<usize>>(&mut self, layer_id: LayerId, index: impl Point2d, value: T) -> T {
+    pub fn set_unchecked<LayerId: Into<usize>>(
+        &mut self,
+        layer_id: LayerId,
+        index: impl Point2d,
+        value: T,
+    ) -> T {
         self.layers[layer_id.into()].set_unchecked(index, value)
     }
-
 
     pub fn get_grid_by_layer<LayerId: Into<usize>>(&self, layer_id: LayerId) -> Option<&Grid<T>> {
         let layer_id = layer_id.into();
@@ -194,5 +288,3 @@ impl<T: GridParam, const LAYER_COUNT: usize> Grid3d<T, LAYER_COUNT> {
         }
     }
 }
-
-
