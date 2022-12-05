a97ccc07a744bf7f2b1ea5a7cb0516cdd3c04f5b
Wesley Feldsine <laocoon7@gmail.com>
2022-12-05 11:25:21 -0800

    new line iter

--- crates/data/src/utilities/geometry/shapes/iter/line_iter.rs
+++ crates/data/src/utilities/geometry/shapes/iter/line_iter.rs
@@ -10,80 +10,71 @@ struct Octant(u8);
 impl Octant {
     /// adapted from <http://codereview.stackexchange.com/a/95551>
     #[inline]
-    const fn from_points(start: Position, end: Position) -> Self {
-        let (start_x, start_y, _start_z) = start.absolute_position();
-        let (end_x, end_y, _end_z) = end.absolute_position();
-
-        let mut delta_x = end_x - start_x;
-        let mut delta_y = end_y - start_y;
-
+    fn from_points(start: (i64, i64), end: (i64, i64)) -> Self {
+        let mut dx = end.0 - start.0;
+        let mut dy = end.1 - start.1;
         let mut octant = 0;
-
-        if delta_y < 0 {
-            delta_x = -delta_x;
-            delta_y = -delta_y;
+        if dy < 0 {
+            dx = -dx;
+            dy = -dy;
             octant += 4;
         }
-        if delta_x < 0 {
-            let tmp = delta_x;
-            delta_x = delta_y;
-            delta_y = -tmp;
+        if dx < 0 {
+            let tmp = dx;
+            dx = dy;
+            dy = -tmp;
             octant += 2;
         }
-        if delta_x < delta_y {
+        if dx < dy {
             octant += 1;
         }
-
         Self(octant)
     }
 
     #[inline]
-    fn to_octant(&self, p: Position) -> Position {
-        let (mut x, mut y, z) = p.absolute_position();
-
-        (x, y) = match self.0 {
-            0 => (x, y),
-            1 => (y, x),
-            2 => (y, -x),
-            3 => (-x, y),
-            4 => (-x, -y),
-            5 => (-y, -x),
-            6 => (-y, x),
-            7 => (x, -y),
+    fn to_octant(&self, p: (i64, i64)) -> (i64, i64) {
+        match self.0 {
+            0 => (p.0, p.1),
+            1 => (p.1, p.0),
+            2 => (p.1, -p.0),
+            3 => (-p.0, p.1),
+            4 => (-p.0, -p.1),
+            5 => (-p.1, -p.0),
+            6 => (-p.1, p.0),
+            7 => (p.0, -p.1),
             _ => unreachable!(),
-        };
-        Position::from_absolute_position((x, y, z), p.layer())
+        }
     }
 
     #[inline]
     #[allow(clippy::wrong_self_convention)]
-    fn from_octant(&self, p: Position) -> Position {
-        let (mut x, mut y, z) = p.absolute_position();
-
-        (x, y) = match self.0 {
-            0 => (x, y),
-            1 => (y, x),
-            2 => (-y, x),
-            3 => (-x, y),
-            4 => (-x, -y),
-            5 => (-y, -x),
-            6 => (y, -x),
-            7 => (x, -y),
+    fn from_octant(&self, p: (i64, i64)) -> (i64, i64) {
+        match self.0 {
+            0 => (p.0, p.1),
+            1 => (p.1, p.0),
+            2 => (-p.1, p.0),
+            3 => (-p.0, p.1),
+            4 => (-p.0, -p.1),
+            5 => (-p.1, -p.0),
+            6 => (p.1, -p.0),
+            7 => (p.0, -p.1),
             _ => unreachable!(),
-        };
-        Position::from_absolute_position((x, y, z), p.layer())
+        }
     }
 }
 
 /// Line-drawing iterator
 #[derive(Debug, Clone)]
 pub struct BresenhamLineIter {
-    delta_x: i32,    // sub from step_ratio when step_ratio is 0/positive
-    delta_y: i32,    // add to step_ratio when step_ratio is negative
-    step_ratio: i32, // determines whether we need to move vertical or horizontal next
-    end_x: i64,      // determins whether we have moved far enough
-    pos: Position,   // our current position
-    octant: Octant,  // the direction we are moving
+    x: i64,
+    y: i64,
+    z: i32,
+    layer: u32,
+    delta_x: i64,
+    delta_y: i64,
+    x1: i64,
+    diff: i64,
+    octant: Octant,
 }
 
 impl BresenhamLineIter {
@@ -91,37 +82,43 @@ impl BresenhamLineIter {
     /// and `end`. Does include `start` but not `end`.
     #[inline]
     pub fn new(start: Position, end: Position) -> Self {
-        let end_x = end.absolute_x();
-        let octant = Octant::from_points(start, end);
-        let start_octant = octant.to_octant(start);
-        let end_octant = octant.to_octant(end);
+        let (s_abs_x, s_abs_y, s_abs_z) = start.absolute_position();
+        let (e_abs_x, e_abs_y, _e_abs_z) = end.absolute_position();
 
-        let delta_x = end_octant.x() as i32 - start_octant.x() as i32;
-        let delta_y = end_octant.y() as i32 - start_octant.y() as i32;
+        let octant = Octant::from_points((s_abs_x, s_abs_y), (e_abs_x, e_abs_y));
+        let start_octant = octant.to_octant((s_abs_x, s_abs_y));
+        let end_octant = octant.to_octant((e_abs_x, e_abs_y));
+
+        let delta_x = end_octant.0 - start_octant.0;
+        let delta_y = end_octant.1 - start_octant.1;
 
         Self {
+            x: start_octant.0,
+            y: start_octant.1,
+            z: s_abs_z,
+            layer: start.layer(),
             delta_x,
             delta_y,
+            x1: end_octant.0,
+            diff: delta_y - delta_x,
             octant,
-            pos: start_octant,
-            end_x,
-            step_ratio: delta_y - delta_x,
         }
     }
 
     /// Return the next point without checking if we are past `end`.
     #[inline]
     pub fn advance(&mut self) -> Position {
-        let pos = self.pos;
-        if self.step_ratio >= 0 {
-            self.step_ratio -= self.delta_x;
-            self.pos.set_y(self.pos.y() + 1);
+        let current_point = (self.x, self.y);
+        if self.diff >= 0 {
+            self.y += 1;
+            self.diff -= self.delta_x;
         }
-        self.step_ratio += self.delta_y;
-
+        self.diff += self.delta_y;
         // loop inc
-        self.pos.set_x(self.pos.x() + 1);
-        self.octant.from_octant(pos)
+        self.x += 1;
+        let (x, y) = self.octant.from_octant(current_point);
+
+        Position::from_absolute_position((x, y, self.z), self.layer)
     }
 }
 
@@ -130,8 +127,7 @@ impl Iterator for BresenhamLineIter {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        let abs_x = self.pos.absolute_x();
-        if abs_x >= self.end_x {
+        if self.x >= self.x1 {
             None
         } else {
             Some(self.advance())
@@ -161,8 +157,7 @@ impl Iterator for BresenhamLineInclusiveIter {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        let (abs_x, _abs_y, _abs_z) = self.0.pos.absolute_position();
-        if abs_x > self.0.end_x {
+        if self.0.x > self.0.x1 {
             None
         } else {
             Some(self.0.advance())
@@ -170,24 +165,85 @@ impl Iterator for BresenhamLineInclusiveIter {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[cfg(test)]
-    mod line {
-        use crate::prelude::*;
-
-        #[test]
-        fn line_vertical() {
-            let p1 = Position::ZERO;
-            let mut p2 = Position::ZERO;
-            p2.set_xy(UVec2::new(0, 10));
-
-            let mut canvas = Canvas::new([11, 11]);
-            let line = Line::new(p1, p2);
-            for p in line.iter() {
-                canvas.put(p.gridpoint(), '*');
-            }
-            canvas.print();
-        }
-    }
-}
+//#[cfg(test)]
+// mod tests {
+//    #[cfg(test)]
+//    mod line {
+//        use crate::prelude::*;
+//
+//        #[test]
+//        fn line_vertical() {
+//            let mut canvas = Canvas::new([11, 11]);
+//            let line = Line::new((0, 0), (0, 10));
+//            for p in line.iter() {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//
+//        #[test]
+//        fn line_horizontal() {
+//            let mut canvas = Canvas::new([11, 11]);
+//            let line = Line::new((0, 0), (10, 0));
+//            for p in line.iter() {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//
+//        #[test]
+//        fn line_diagonal() {
+//            let mut canvas = Canvas::new([10, 10]);
+//            let line = Line::new((0, 0), (9, 9));
+//            for p in line.iter() {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//
+//        #[test]
+//        fn line_multi() {
+//            let mut canvas = Canvas::new([11, 7]);
+//            let lines = [
+//                Line::new((5, 3), (10, 5)),
+//                Line::new((5, 3), (10, 1)),
+//                Line::new((5, 3), (0, 1)),
+//                Line::new((5, 3), (0, 5)),
+//            ];
+//            for p in lines.iter().flat_map(|l| l.iter()) {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//    }
+//
+//    #[cfg(test)]
+//    mod bresenham {
+//        use crate::prelude::*;
+//
+//        #[test]
+//        fn line_diagonal() {
+//            let mut canvas = Canvas::new([10, 10]);
+//            let line = BresenhamLineIter::new((0, 0), (9, 9));
+//            for p in line {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//    }
+//
+//    #[cfg(test)]
+//    mod bresenham_inclusive {
+//        use crate::prelude::*;
+//
+//        #[test]
+//        fn line_diagonal() {
+//            let mut canvas = Canvas::new([10, 10]);
+//            let line = BresenhamLineInclusiveIter::new((0, 0), (9, 9));
+//            for p in line {
+//                canvas.put(p, '*');
+//            }
+//            canvas.print();
+//        }
+//    }
+//}
