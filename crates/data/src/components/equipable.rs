beff395d1c350c689eb85fe9c51caecbd5763bb6
Wesley Feldsine <laocoon7@gmail.com>
2022-12-07 11:06:08 -0800

    cleanup

--- crates/data/src/components/equipable.rs
+++ crates/data/src/components/equipable.rs
@@ -2,6 +2,6 @@ use crate::prelude::*;
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 pub struct Equipable {
-    pub location: EquipmentSlot,
+    pub location: Option<EquipmentSlot>,
     pub available_locations: Vec<EquipmentSlot>,
 }
--- crates/data/src/components/tags.rs
+++ crates/data/src/components/tags.rs
@@ -1,15 +1,5 @@
 use crate::prelude::*;
 
-// Using Res<PlayerEntity> to track the difference
-
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 pub struct Mob;
-
-// #[derive(Reflect, Component, Default)]
-// #[reflect(Component)]
-// pub struct Player;
-//
-// #[derive(Reflect, Component, Default)]
-// #[reflect(Component)]
-// pub struct Mob;
